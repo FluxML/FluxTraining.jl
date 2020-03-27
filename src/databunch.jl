1095cbe4ae2601da54db8bf881bf53a173a5f35e
lorenzoh <lorenz.ohly@gmail.com>
2020-03-27 11:07:17 +0100

    removed old code

--- src/databunch.jl
+++
@@ -1,7 +0,0 @@
-mutable struct DataBunch
-    traindl::DataLoader
-    valdl::DataLoader
-    testdl::Union{Nothing, DataLoader}
-    DataBunch(traindl, valdl, testdl = nothing) = new(
-        traindl, valdl, testdl)
-end
