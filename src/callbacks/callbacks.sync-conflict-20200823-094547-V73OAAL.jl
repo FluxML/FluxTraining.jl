94b3a3b5218a981551eae08bd83194dbefee0f14
lorenzoh <lorenz.ohly@gmail.com>
2020-08-28 17:32:26 +0200

    format

---
+++ src/callbacks/callbacks.sync-conflict-20200823-094547-V73OAAL.jl
@@ -0,0 +1,80 @@
+
+# ProgressBarLogger
+
+mutable struct ProgressBarLogger <: AbstractCallback
+    p::Union{Nothing,Progress}
+end
+ProgressBarLogger() = ProgressBarLogger(nothing)
+
+function on(::EpochBegin,
+        phase::AbstractFittingPhase,
+        cb::ProgressBarLogger,
+        learner)
+    e = learner.state.history.epochs + 1
+    cb.p = Progress(numsteps(learner, phase), "Epoch $(e) $(phase): ")
+end
+
+on(::BatchEnd, ::AbstractFittingPhase, cb::ProgressBarLogger, learner) = next!(cb.p)
+
+
+# PrintMetrics
+
+struct PrintMetrics <: AbstractCallback end
+
+function on(::EpochEnd,
+        phase::AbstractFittingPhase,
+        cb::PrintMetrics,
+        learner)
+    cbs = learner.state.callbacks
+    for metric in [cbs.loss, cbs.metrics...]
+        println(string(metric), ": ", value(metric))
+    end
+end
+
+
+# StopOnNaNLoss
+
+struct StopOnNaNLoss <: AbstractCallback end
+
+function on(::BackwardEnd, ::AbstractTrainingPhase, ::StopOnNaNLoss, learner)
+    !isnan(learner.state.batch.loss) || throw(CancelFittingException("Encountered NaN loss"))
+end
+
+
+# Early stopping
+mutable struct EarlyStopping <: AbstractCallback
+    patience::Int
+    waited::Int
+    lowest::Float64
+end
+EarlyStopping(patience) = EarlyStopping(patience, 0, Inf64)
+
+function on(::EpochEnd, ::ValidationPhase, cb::EarlyStopping, learner)
+    valloss = value(learner.state.callbacks.loss)
+    if (valloss > cb.lowest)
+        if !(cb.waited < cb.patience)
+            throw(CancelFittingException("Validation loss did not improve for $(cb.patience) epochs"))
+        else
+            cb.waited += 1
+        end
+    else
+        cb.waited = 0
+        cb.lowest = valloss
+    end
+end
+
+
+
+garbagecollect() = (GC.gc(); ccall(:malloc_trim, Cvoid, (Cint,), 0))
+
+"""
+    GarbageCollect(nsteps)
+
+Every `nsteps`, forces garbage collection.
+Use this if you get memory leaks from, for example, parallel data loading.
+"""
+function GarbageCollect(nsteps::Int = 100)
+    return CustomCallback{BatchEnd, AbstractFittingPhase}(nsteps) do learner
+        garbagecollect()
+    end
+end
--- src/callbacks/customcallback.jl
+++ src/callbacks/customcallback.jl
@@ -1,13 +1,18 @@
 
-mutable struct CustomCallback{E <: FitEvent,P <: AbstractFittingPhase} <: AbstractCallback
-    fn
+mutable struct CustomCallback{E<:FitEvent,P<:AbstractFittingPhase} <: AbstractCallback
+    fn::Any
     everyn::Int
     _current::Int
     CustomCallback{E,P}(fn, everyn = 1) where {E,P} = new{E,P}(fn, everyn, 0)
 end
 
 
-function on(::E, ::P, cb::CustomCallback{E,P}, learner) where {E <: FitEvent,P <: AbstractFittingPhase}
+function on(
+    ::E,
+    ::P,
+    cb::CustomCallback{E,P},
+    learner,
+) where {E<:FitEvent,P<:AbstractFittingPhase}
     cb._current += 1
     if cb._current % cb.everyn == 0
         cb.fn(learner)
