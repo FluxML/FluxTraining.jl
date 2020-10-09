80df6b3fab3dbb4e0613cd7c910412675872d344
lorenzoh <lorenz.ohly@gmail.com>
2020-10-09 11:20:25 +0200

    remove old docs

--- 0.1.0/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/FluxTraining.jl/0.1.0';
-var PUBLISH_VERSION = "0.1.0";
-var PUBLISH_VERSIONS = [
-    ["dev","/FluxTraining.jl/dev/index.html"],
-    ["0.1.0","/FluxTraining.jl/0.1.0/index.html"],
-];
--- dev/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/FluxTraining.jl/dev';
-var PUBLISH_VERSION = "dev";
-var PUBLISH_VERSIONS = [
-    ["dev","/FluxTraining.jl/dev/index.html"],
-    ["0.1.0","/FluxTraining.jl/0.1.0/index.html"],
-];
