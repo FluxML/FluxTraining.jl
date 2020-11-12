60d8667727cb217aee07dfe9a3803a9df3456e9d
lorenzoh <lorenz.ohly@gmail.com>
2020-11-12 10:01:40 +0100

    new test dependency

--- test/imports.jl
+++ test/imports.jl
@@ -2,6 +2,7 @@ using Test
 using TestSetExtensions
 using FluxTraining
 using Animations
+using Colors
 using FluxTraining: EpochEnd, LearningRate, getdataiter, protect, Protected,
     ProtectedException, Read, Write, Events, Phases
 using Flux: trainable
