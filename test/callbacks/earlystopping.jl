452c93ae3e8733fcf07933a6163d1598e8e4531c
lorenzoh <lorenz.ohly@gmail.com>
2021-02-22 17:49:53 +0100

    make test more robust

--- test/callbacks/earlystopping.jl
+++ test/callbacks/earlystopping.jl
@@ -2,7 +2,7 @@ include("../imports.jl")
 
 
 @testset ExtendedTestSet "StopEarly" begin
-    learner = testlearner(Recorder(), Metrics(), EarlyStopping(3), coeff = 3, opt = Descent(10.))
-    fit!(learner, 10)
-    @test learner.cbstate.history[TrainingPhase()].epochs < 10
+    learner = testlearner(Recorder(), Metrics(), EarlyStopping(2), coeff = 3, opt = Descent(10.))
+    fit!(learner, 100)
+    @test learner.cbstate.history[TrainingPhase()].epochs < 100
 end
