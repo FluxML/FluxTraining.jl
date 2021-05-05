9abc62174e587c39be7ad5e16680ba65d8a7e848
lorenzoh <lorenz.ohly@gmail.com>
2021-05-05 19:39:01 +0200

    fix test

--- test/callbacks/earlystopping.jl
+++ test/callbacks/earlystopping.jl
@@ -2,7 +2,7 @@ include("../imports.jl")
 
 
 @testset ExtendedTestSet "StopEarly" begin
-    learner = testlearner(Recorder(), Metrics(), EarlyStopping(2), coeff = 3, opt = Descent(10.))
+    learner = testlearner(Recorder(), Metrics(), EarlyStopping(1), ProgressPrinter(), coeff = 3, opt = Descent(0.1))
     fit!(learner, 100)
     @test learner.cbstate.history[TrainingPhase()].epochs < 100
 end
