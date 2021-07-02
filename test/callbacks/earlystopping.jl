2261e50e68a94c4c903106cf31765c2db9aacd1d
lorenzoh <lorenz.ohly@gmail.com>
2021-07-02 10:41:15 +0200

    suppress test output

--- test/callbacks/earlystopping.jl
+++ test/callbacks/earlystopping.jl
@@ -3,5 +3,7 @@ include("../imports.jl")
 
 @testset ExtendedTestSet "StopEarly" begin
     learner = testlearner(Recorder(), Metrics(), EarlyStopping(1), ProgressPrinter(), coeff = 3, opt = Descent(0.1))
-    @test_throws CancelFittingException fit!(learner, 100)
+    @test_throws CancelFittingException begin
+        @suppress fit!(learner, 100)
+    end
 end
