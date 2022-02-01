9036cf30630a968707c0eddfcb54ed0b7ba26aa8
Avik Pal <avikpal@iitk.ac.in>
2022-02-01 07:53:13 -0500

    counting issue

--- src/functional/metrics.jl
+++ src/functional/metrics.jl
@@ -1,8 +1,8 @@
 # FIXME: Metric(Accuracy) will give incorrect results and cannot be report in papers.
 function accuracy(y_pred, y)
-    matches = onecold(y_pred) .== onecold(y)
-    match_count = sum(matches)
-    count = length(y)
+    y_pred_oc, y_oc = onecold(y_pred), onecold(y)
+    match_count = sum(y_pred_oc .== y_oc)
+    count = length(y_oc)
     if is_distributed_data_parallel()
         res = [match_count, count]
         clean_println(res)
