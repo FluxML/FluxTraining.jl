2eb8c1fb5333594e1e69f9a6a1d8d07e203e5d5d
lorenzoh <lorenz.ohly@gmail.com>
2022-02-23 14:23:09 +0100

    Add and improve a lot of docstrings and a few test cases (#99)
    * Add tests to `throttle`
    
    * Add usage examples to `throttle`
    
    * Add more docstrings for callback API
    
    * Add more docstrings for training API
    
    * Add docstring for callback conditions
    
    * Add usage example to docstring of `CustomCallback`
    
    * Add test case for `CustomCallback`
    
    * More callback docs improvements
    
    * more docs
    
    * Event and state reference for phases
    
    * better docstrings for `epoch!` and `step!`
    
    * Example for `onecycle`
    
    * Fix `GarbageCollect`
    
    * exclude broken test on windows

--- src/callbacks/conditional.jl
+++ src/callbacks/conditional.jl
@@ -2,13 +2,32 @@
 # High-level interface
 
 """
-    throttle(callback, event, freq = 1)
-    throttle(callback, event, seconds = 1)
+    throttle(callback, Event, freq = 1)
+    throttle(callback, Event, seconds = 1)
 
-Throttle `event` for `callback` so that it is triggered either only every
+Throttle `Event` type for `callback` so that it is triggered either only every
 `freq`'th time  or every `seconds` seconds.
+
+## Examples
+
+If you want to only sporadically log metrics ([`LogMetrics`](#)) or images
+([`LogVisualization`](#)), `throttle` can be used as follows.
+
+Every 10 steps:
+
+```julia
+callback = throttle(LogMetrics(TensorBoardBackend()), StepEnd, freq = 10)
+learner = Learner(<args>, callback)
+```
+
+Or every 5 seconds:
+
+```julia
+callback = throttle(LogMetrics(TensorBoardBackend()), StepEnd, seconds = 5)
+learner = Learner(<args>, callback)
+```
 """
-function throttle(callback, event; freq = nothing, seconds = nothing)
+function throttle(callback, event::Type{<:Event}; freq = nothing, seconds = nothing)
     xor(isnothing(freq), isnothing(seconds)) || error("Pass either `every` OR `seconds`.")
     if !isnothing(freq)
         return ConditionalCallback(callback, FrequencyThrottle(freq, event))
@@ -19,14 +38,30 @@ end
 
 # Implementation
 
+"""
+    abstract type CallbackCondition
+
+Supertype for conditions to use with [`ConditionalCallback`](#).
+To implement a `CallbackCondition`, implement
+[`shouldrun`](#)`(::MyCondition, event, phase)`.
+
+See [`FrequencyThrottle`](#), [`TimeThrottle`](#) and [`throttle`](#).
+"""
 abstract type CallbackCondition end
 
+"""
+    ConditionalCallback(callback, condition) <: Callback
+
+Wrapper callback that only forwards events to the wrapped callback
+if [`CallbackCondition`](#) `condition` is met. See [`throttle`](#).
+"""
 struct ConditionalCallback <: Callback
     callback::Callback
     condition::CallbackCondition
 end
 
 stateaccess(cc::ConditionalCallback) = stateaccess(cc.callback)
+init!(cc::ConditionalCallback, learner) = init!(cc.callback, learner)
 
 function on(event::Event, phase::Phase, cb::ConditionalCallback, learner)
     if shouldrun(cb.condition, event, phase)
@@ -39,7 +74,7 @@ mutable struct FrequencyThrottle <: CallbackCondition
     freq
     event
     counter
-    FrequencyThrottle(f, e, c = 1) = new(f, e, c)
+    FrequencyThrottle(f, e) = new(f, e, 1)
 end
 
 function shouldrun(c::FrequencyThrottle, event, phase)
@@ -74,5 +109,27 @@ function shouldrun(c::TimeThrottle, event, phase)
         end
     else
         return true
+    end
+end
+
+
+@testset "throttle" begin
+    function train(cb)
+        learner = testlearner(cb)
+        epoch!(learner, TrainingPhase())
+        return learner.cbstate.history[TrainingPhase()]
+    end
+
+    @test train(Recorder()).steps == 16
+    @testset "freq" begin
+        @test train(throttle(Recorder(), StepEnd, freq = 2)).steps == 8
+        @test train(throttle(Recorder(), StepEnd, freq = 16)).steps == 1
+    end
+
+    if !Base.Sys.iswindows()
+        @testset "seconds" begin
+            @test train(throttle(Recorder(), StepEnd, seconds = 0)).steps == 16
+            @test train(throttle(Recorder(), StepEnd, seconds = 10)).steps == 1
+        end
     end
 end
