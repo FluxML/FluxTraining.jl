6ba98f8134fb0ab19dee8659a26261db264f566a
rejuvyesh <mail@rejuvyesh.com>
2022-04-02 16:49:44 +0000

    fix docstring

--- src/callbacks/scheduler.jl
+++ src/callbacks/scheduler.jl
@@ -2,7 +2,7 @@
     Scheduler(schedules...)
 
 Callback for hyperparameter scheduling. Takes pairs of [`HyperParameter`](#)
-types and [`Schedule`](#)s.
+types and [ParameterSchedulers.jl schedules](https://darsnack.github.io/ParameterSchedulers.jl/dev/README.html).
 
 See [the tutorial](/documents/docs/tutorials/hyperparameters.md) for more information.
 
