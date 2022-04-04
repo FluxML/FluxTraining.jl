61cc4a3e5534d0bf23827aac695cf226942c9b36
Jayesh K. Gupta <mail@rejuvyesh.com>
2022-04-03 23:13:19 -0700

    Switch to ParameterSchedulers.jl (#107)
    * move to ParameterSchedulers
    
    * update docs
    
    * Bring back fixed `onecycle`
    
    * fix docs
    
    * recommend PS.jl docs for details
    
    Co-authored-by: darsnack <daruwalla@wisc.edu>
    Co-authored-by: lorenzoh <lorenz.ohly@gmail.com>

--- test/callbacks/scheduler.jl
+++ test/callbacks/scheduler.jl
@@ -3,12 +3,12 @@ include("../imports.jl")
 
 @testset "`Scheduler`" begin
     nbatches = 16
-    schedule = Schedule([0, 2], [1e-4, 1e-6], Animations.linear())
+    schedule = Interpolator(Poly(λ=1e-4, p=1, max_iter=10), nbatches)
     learner = testlearner(coeff = 3, Recorder(), Scheduler(LearningRate => schedule), nbatches = nbatches)
 
     epoch!(learner, TrainingPhase())
-    @test isapprox(last(learner.cbstate.hyperparams[:LearningRate])[2], ((1e-4 + 1e-6) / 2), atol = 0.1)
+    @test isapprox(last(learner.cbstate.hyperparams[:LearningRate])[2], 9.0e-5, atol = 0.1)
 
     epoch!(learner, TrainingPhase())
-    @test isapprox(last(learner.cbstate.hyperparams[:LearningRate])[2], 1e-6, atol =0.1)
+    @test isapprox(last(learner.cbstate.hyperparams[:LearningRate])[2], 8.0e-5, atol =0.1)
 end
